05796d52c29e3e8a963678207fe869e9a9732dfc
UrOkaSan <tashtanaliev11@mail.ru>
2023-07-31 15:30:39 +0600

    Initial Commit

---
+++ GLORIA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GLORIA
+//
+//  Created by User on 31/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
